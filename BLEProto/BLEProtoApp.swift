8cb02270bca09196eda560f7c1fd56fa69efa682
Joseph G. Noonan <jgnoonan@att.net>
2024-01-18 14:58:26 -0500

    Initial Commit

---
+++ BLEProto/BLEProtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEProtoApp.swift
+//  BLEProto
+//
+//  Created by Joseph Noonan on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BLEProtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
